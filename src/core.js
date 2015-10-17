dd1d989ec614b94a420fb37cd608c44dcf871ee5
Kieran Boyle <kboyle@yahoo-inc.com>
2015-10-17 14:35:35 -0700

    add live editor; add val to core;

--- src/core.js
+++ src/core.js
@@ -893,6 +893,27 @@
         }
 
         return this[(this.hasClass(cls) ? 'removeClass' : 'addClass')](fn && fn(cls) || cls);
+      },
+      /**
+       * Gets the value for the first element in the matched set or sets the value for one or more elements
+       * @param  {Mixed} value The value to set
+       * @return {Mixed}       The property value
+       */
+      val: function(value){
+
+        if(this[0] === undefined){
+          return;
+        }
+
+        if(value === undefined){
+          return this[0].value;
+        }
+
+        for(var i = 0, k = this.length; i < k; i++){
+          this[i].value = value;
+        }
+
+        return this;
       }
     });
 
