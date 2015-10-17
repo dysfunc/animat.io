900559d4e5b8c196701de6bf50a2e0b0d575cfbc
Kieran Boyle <kboyle@yahoo-inc.com>
2015-10-17 13:40:06 -0700

    Updates

--- js/demo.js
+++ js/demo.js
@@ -1,54 +1,54 @@
-animatio(function(){
+$(function(){
 
   gumshoe.init();
 
-  var buttons = animatio('.btn').each(function(){
-    this.addEventListener('click', function(){
+  var buttons = $('.btn').each(function(){
+    $(this).on('click', function(){
 
-      var img = animatio(this).parent().parent().parent().find('.html-badge'),
-          animation = animatio(this).data('effect');
+      var img = $(this).parent().parent().parent().find('.html-badge'),
+          animation = $(this).data('effect');
 
       img.animate(animation, function(){
-        animatio(this).animate('reset');
+        $(this).animate('reset');
       });
     });
   });
 
-  animatio('#growl').on('click', function(){
-    animatio('#notification').animate('fadeInUpBig', { duration: 750 }, function(){
+  $('#growl').on('click', function(){
+    $('#notification').animate('fadeInUpBig', { duration: 750 }, function(){
       this.style.opacity = 1;
 
-      animatio(this).animate('fadeOutUpBig', {
+      $(this).animate('fadeOutUpBig', {
         delay: 750,
         duration: 750
       });
     });
   });
 
-  animatio('#animate_box').on('click',  function(){
-    animatio('#animate_me').animate('lightSpeedIn', function(){
-      animatio(this).animate('lightSpeedOut', function(){
-        animatio(this).animate('flipInX', function(){
+  $('#animate_box').on('click',  function(){
+    $('#animate_me').animate('lightSpeedIn', function(){
+      $(this).animate('lightSpeedOut', function(){
+        $(this).animate('flipInX', function(){
           //not relative
-          animatio(this)
+          $(this)
             .animate('swing')
             .transform({ left: '20%', top: 60, opacity: 1, width: 500, height: 200 }, 1000, function(){
             //relative string
-            animatio(this).animate('flipInX').transform({ left: '-=100', top: '+=60', opacity: 1, width: '+=500', height: 200 }, 1000, function(){
+            $(this).animate('flipInX').transform({ left: '-=100', top: '+=60', opacity: 1, width: '+=500', height: 200 }, 1000, function(){
               //relative stringify
-              animatio(this).animate('swing').transform({ left: '-=' + 100, top: '+=' + 60, opacity: 1, width: '+=' + 500, height: 200 }, 1000, function(){
-                animatio(this).transform({ left: '50%', top: '+=' + 60, borderRadius: '4px', width: '-=' + 800, height: 80}, 1000, function(){
-                  animatio(this).animate('flipOutX', function(){
-                    animatio(this).animate('flipInY', function(){
-                      animatio(this).transform({ left: '-=100', top: '+=' + 60, opacity: 1, width: '+=' + 500, height: 500 }, 1000, function(){
-                        animatio(this).transform({ left: '50', borderRadius: '2px', top: 40, width: '50%', height: 100}, 1500, function(){
-                          animatio(this).transform({ left: '50%', top: '50%', width: 50, height: 50, borderRadius: 0, opacity: 1 }, 1500);
+              $(this).animate('swing').transform({ left: '-=' + 100, top: '+=' + 60, opacity: 1, width: '+=' + 500, height: 200 }, 1000, function(){
+                $(this).transform({ left: '50%', top: '+=' + 60, borderRadius: '4px', width: '-=' + 800, height: 80}, 1000, function(){
+                  $(this).animate('flipOutX', function(){
+                    $(this).animate('flipInY', function(){
+                      $(this).transform({ left: '-=100', top: '+=' + 60, opacity: 1, width: '+=' + 500, height: 500 }, 1000, function(){
+                        $(this).transform({ left: '50', borderRadius: '2px', top: 40, width: '50%', height: 100}, 1500, function(){
+                          $(this).transform({ left: '50%', top: '50%', width: 50, height: 50, borderRadius: 0, opacity: 1 }, 1500);
                         });
                       });
                     });
                   });
 
-                  animatio(this).transform({ borderRadius: 0, width: 50, height: 50 }, 1000);
+                  $(this).transform({ borderRadius: 0, width: 50, height: 50 }, 1000);
                 });
               });
             });
