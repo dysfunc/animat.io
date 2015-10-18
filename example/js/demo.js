e9895d75238a453116bf01ed1954b720a041eb7f
Kieran Boyle <kboyle@yahoo-inc.com>
2015-10-17 18:48:02 -0700

    add demo source

---
+++ example/js/demo.js
@@ -0,0 +1,38 @@
+$(function(){
+
+  gumshoe.init();
+
+  $('#splash').find('h1').animate('vanishIn', function(){
+    $('#splash').find('h2').animate('boingInUp', function(){
+      $('#download-now').animate('lightSpeedIn');
+    });
+  });
+
+  $('#download-now').click(function(){
+    window.location.href = "http://github.com/dysfunc/animat.io/zipball/master/"
+  });
+
+  var buttons = $('.btn').each(function(){
+    $(this).on('click', function(){
+
+      var img = $(this).parent().parent().parent().find('.html-badge'),
+          animation = $(this).data('effect');
+
+      img.animate(animation, function(){
+        $(this).animate('reset');
+      });
+    });
+  });
+
+  $('#growl').on('click', function(){
+    var code = $('#notification-code').val();
+
+    return eval(code)(1);
+  });
+
+  $('#animate_box').on('click',  function(){
+    var code = $('#transform-code').val();
+
+    return eval(code)(1);
+  });
+});
